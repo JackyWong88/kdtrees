dc8151e764cd749b300c49a95b70a45841ad86d2
Jacky <Jacky@10.210.1.28>
2016-03-28 19:17:16 +0900

    Added A LOT. Created Node class and its subclasses with all its methods but drawing the line. Implemented insert and contains but have not tested them. Working on draw() - still needs to draw lines. After that, can start on debugging some and then implement the other methods.

--- kdtrees/src/KdTree.java
+++ kdtrees/src/KdTree.java
@@ -1,9 +1,9 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.BST;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,33 +16,194 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Jacky
  */
 public class KdTree {
-    private BST<Node, Integer> tree;
     private int count = 0;
+    private XNode root;
+    
+    private static abstract class Node{
+        private Point2D p;
+        private Node left, right;  // left and right subtrees
+        private int val;             // number of nodes in subtree
+        
+        abstract int compareTo(Point2D p);
+        abstract int compareTo(RectHV rect);
+        abstract Node makeNode(Point2D p, int n);
+        //abstract RectHV rectCompute(RectHV parentRect, String side);
+        abstract void setPenColor();
+        //abstract void lineThroughPoint(Point2D point);
+        abstract void drawLine(Point2D point);
+        
+        private Node(Point2D key, int val) {
+            this.p = key;
+            this.val = val;
+        }
+    }
+    
+    private class YNode extends Node{
+        private Point2D p;
+        private Node left, right, parent;  // left and right subtrees
+        private int N;             // number of nodes in subtree
+        
+        private YNode(Point2D p, int n) {
+            super(p, n);
+        }
+
+        @Override
+        int compareTo(Point2D p) {
+            if (this.p.y() < p.y()) return -1;
+            if (this.p.y() > p.y()) return 1;
+            return 0;
+        }
+
+        @Override
+        int compareTo(RectHV rect) {
+            if (rect.ymin() > this.p.y()) return -1;
+            if (rect.ymax() < this.p.y()) return 1;
+            return 0;
+        }
+
+        @Override
+        Node makeNode(Point2D p, int n){
+            return new XNode(p, n);
+        }
+
+        public int compareTo(Node that) {
+            if (this.p.y() < that.p.y()) return -1;
+            if (this.p.y() > that.p.y()) return 1;
+            return 0;
+        }
+
+        @Override
+        void setPenColor() {
+            StdDraw.setPenColor(StdDraw.RED);
+        }
+
+        @Override
+        void drawLine(Point2D point) {
+            
+        }
+    }
+
+    private class XNode extends Node{
+        private Point2D p;
+        private Node left, right, parent;  // left and right subtrees
+        private int N;             // number of nodes in subtree
+        
+        private XNode(Point2D p, int n) {
+            super(p, n);
+        }
+
+        @Override
+        int compareTo(Point2D p) {
+            if (this.p.x() < p.x()) return -1;
+            if (this.p.x() > p.x()) return 1;
+            return 0;
+        }
+
+        @Override
+        int compareTo(RectHV rect) {
+            if (rect.xmin() > this.p.x()) return -1;
+            if (rect.xmax() < this.p.x()) return 1;
+            return 0;
+        }
+
+        @Override
+        Node makeNode(Point2D p, int n){
+            return new YNode(p, n);
+        }
+
+        public int compareTo(Node that) {
+            if (this.p.x() < that.p.x()) return -1;
+            if (this.p.x() > that.p.x()) return 1;
+            return 0;
+        }
+
+        @Override
+        void setPenColor() {
+            StdDraw.setPenColor(StdDraw.BLUE);
+        }
+
+        @Override
+        void drawLine(Point2D point) {
+            
+        }
+    }
     
     public KdTree() {                              // construct an empty set of points 
-        tree = new BST<Node, Integer>();
+        
     }
 
     public boolean isEmpty() {                     // is the set empty? 
-        return tree.isEmpty();
+        return count == 0;
     }
 
     public int size() {                        // number of points in the set 
-        return tree.size();
+        return count;
     }
 
     public void insert(Point2D p) {             // add the point to the set (if it is not already in the set)
-        Node node = new Node(p, count);
-        tree.put(node, count);
-        count++;
+        if (isEmpty()) {
+            root = new XNode(p, count);
+            count++;
+        } else {
+            Node node = root;
+            Node parent = null;
+            while(node != null) {
+                int cmp = node.compareTo(p);
+                if (cmp > 0) {
+                    parent = node;
+                    node = node.left;
+                }
+                if (cmp < 0) {
+                    parent = node;
+                    node = node.right;
+                } else return;
+            }
+            
+            int cmp = parent.compareTo(p);
+            if (cmp > 0) {
+                parent.left = parent.makeNode(p, count);
+                count++;
+            } else {
+                parent.right = parent.makeNode(p, count);
+                count++;
+            }
+        }
     }
 
     public boolean contains(Point2D p) {           // does the set contain point p? 
+        Node node = root;
+        while(node != null) {
+            int cmp = node.compareTo(p);
+            if (cmp > 0) node = node.left;
+            else if (cmp < 0) node = node.right;
+            else return true;
+        }
         return false;
     }
 
     public void draw() {                        // draw all points to standard draw 
-        
+        if(isEmpty()) return;
+        Node max = root;
+        while(max.right != null) {
+            max = max.right;
+        }
+        Node min = root;
+        while(min.left != null) {
+            min = min.left;
+        }
+        draw(root, min.p, max.p, null);
+    }
+    
+    private void draw(Node x, Point2D lo, Point2D hi, Node parent) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.p);
+        int cmphi = hi.compareTo(x.p);
+        if (cmplo < 0) draw(x.left, lo, hi, x);
+        if (cmplo <= 0 && cmphi >= 0) {
+            x.p.draw();
+            x.drawLine(parent.p);
+        }
+        if (cmphi > 0) draw(x.right, lo, hi, x);
     }
 
     public Iterable<Point2D> range(RectHV rect) {            // all points that are inside the rectangle 
@@ -55,75 +216,16 @@ public class KdTree {
         return champion;
     }
     
-    private abstract class Node{
-        private boolean horizontal = true;
-        Point2D p;
-        
-        abstract int compareTo(Point2D p);
-        abstract int compareTo(RectHV rect);
-        abstract Node makeNode(Point2D p, int n);
-        //abstract RectHV rectCompute(RectHV parentRect, String side);
-        //abstract void setPenColor();
-        //abstract void lineThroughPoint(Point2D point);
-        
-        private Node(Point2D p, int n) {
-            
-        }
-        
-        private class YNode extends Node{
-            private YNode(Point2D p, int n) {
-                super(p, n);
-            }
-            
-            int compareTo(Point2D p) {
-                if (this.p.y() < p.y()) return -1;
-                if (this.p.y() > p.y()) return 1;
-                return 0;
-            }
-                    
-            int compareTo(RectHV rect) {
-                if (rect.ymin() > this.p.y()) return -1;
-                if (rect.ymax() < this.p.y()) return 1;
-                return 0;
-            }
-            
-            Node makeNode(Point2D p, int n){
-                return new XNode(p, n);
-            }
-        }
-        
-        private class XNode extends Node{
-            private XNode(Point2D p, int n) {
-                super(p, n);
-            }
-            
-            int compareTo(Point2D p) {
-                if (this.p.x() < p.x()) return -1;
-                if (this.p.x() > p.x()) return 1;
-                return 0;
-            }
-                    
-            int compareTo(RectHV rect) {
-                if (rect.xmin() > this.p.x()) return -1;
-                if (rect.xmax() < this.p.x()) return 1;
-                return 0;
-            }
-            
-            Node makeNode(Point2D p, int n){
-                return new YNode(p, n);
-            }
-        }
-    }
     
     private static int binlog( int bits ) // returns 0 for bits=0
-{
-    int log = 0;
-    if( ( bits & 0xffff0000 ) != 0 ) { bits >>>= 16; log = 16; }
-    if( bits >= 256 ) { bits >>>= 8; log += 8; }
-    if( bits >= 16  ) { bits >>>= 4; log += 4; }
-    if( bits >= 4   ) { bits >>>= 2; log += 2; }
-    return log + ( bits >>> 1 );
-}
+    {
+        int log = 0;
+        if( ( bits & 0xffff0000 ) != 0 ) { bits >>>= 16; log = 16; }
+        if( bits >= 256 ) { bits >>>= 8; log += 8; }
+        if( bits >= 16  ) { bits >>>= 4; log += 4; }
+        if( bits >= 4   ) { bits >>>= 2; log += 2; }
+        return log + ( bits >>> 1 );
+    }
 
     public static void main(String[] args) {                 // unit testing of the methods (optional) 
         int log = binlog(1025);
